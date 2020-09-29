a3dd34b892c67bc88d484b0be60cecfa37d94149
Milena <milena.delfini@gmail.com>
2020-09-29 22:30:33 +0200

    Add Loader component

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const Loader = styled(CircularProgress)`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+`;
+
+export default Loader;
